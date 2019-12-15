bb16340b69fa05e725f352ce15e79acf392e13a7
Iñigo Crespo <inigo.crespo@mail.com>
2019-12-15 22:21:25 +0100

    xxx

--- into/src/main.rs
+++ into/src/main.rs
@@ -1,3 +1,27 @@
+struct Person {
+    name: String,
+}
+
+impl Person {
+    //    fn new(name: &str) -> Person {
+    //        Person{ name: name.to_string()}
+    //    }
+    //fn new<S: Into<String>>(name: S) -> Person {
+    fn new<S>(name: S) -> Person
+    where
+        S: Into<String>,
+    {
+        Person { name: name.into() }
+    }
+}
+
+fn into() {
+    let john = Person::new("John");
+
+    let name = "Jane".to_string();
+    let jane = Person::new(name);
+}
+
 fn main() {
-    println!("Hello, world!");
+    into();
 }
