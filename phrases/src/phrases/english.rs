173f73623af6aed51a7fe42345361b9d7ed6912c
Iñigo Crespo <inigo.crespo@mail.com>
2019-12-20 19:29:54 +0100

    xxx

--- phrases/src/phrases/english.rs
+++ phrases/src/phrases/english.rs
@@ -1,4 +1,14 @@
+//! This is english
+//!
+//! # Examples
+//! ```
+//! let username = "Jhon";
+//! println!("{}", username)
+//! ```
+//!
 
+
+/// say hello
 pub fn say_hello() -> String {
     "Hello".into()
 }
